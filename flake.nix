d257bf2a484e9c123d442b876505639a5ecd3c2a
Bill Noble <bill.noble@gu.se>
2024-02-02 16:01:00 +0100

    initial commit

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    haskell-flake.url = "github:srid/haskell-flake";
+  };
+  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = nixpkgs.lib.systems.flakeExposed;
+      imports = [ inputs.haskell-flake.flakeModule ];
+
+      perSystem = { self', pkgs, ... }: {
+
+        haskellProjects.default = {
+          devShell = {
+            enable = true;
+            tools = hp: { };
+            hlsCheck.enable = true;
+           };
+        };
+
+        packages.default = self'.packages.milling;
+      };
+    };
+}
